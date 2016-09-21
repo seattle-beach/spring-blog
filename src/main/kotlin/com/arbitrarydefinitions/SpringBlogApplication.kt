712b1ec27cf39bd3e03a580380d20afcba158f15
Alpha Chen <achen@pivotal.io>
2016-09-21 11:38:41 -0700

    Initial commit

---
+++ src/main/kotlin/com/arbitrarydefinitions/SpringBlogApplication.kt
@@ -0,0 +1,11 @@
+package com.arbitrarydefinitions
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+open class SpringBlogApplication
+
+fun main(args: Array<String>) {
+    SpringApplication.run(SpringBlogApplication::class.java, *args)
+}
