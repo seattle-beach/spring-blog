a1f86f1813fdbe795d1740a7796d53b9ec7b5dde
Alpha Chen <achen@pivotal.io>
2016-09-21 17:35:38 -0700

    A form for creating Articles
    Signed-off-by: Gregg Van Hove <gvanhove@pivotal.io>

--- src/main/kotlin/com/arbitrarydefinitions/ArticleController.kt
+++ src/main/kotlin/com/arbitrarydefinitions/ArticleController.kt
@@ -1,7 +1,9 @@
 package com.arbitrarydefinitions
 
 import org.springframework.stereotype.Controller
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestMethod
 
 @Controller
 @RequestMapping("/articles/*")
@@ -9,5 +11,15 @@ class ArticleController {
     @RequestMapping("new")
     fun new(): String {
         return "articles/new"
+    }
+
+    @RequestMapping(path = arrayOf("/"), method = arrayOf(RequestMethod.POST))
+    fun create(): String {
+        return "redirect:/articles/omg"
+    }
+
+    @RequestMapping("/{id}")
+    fun show(@PathVariable id: String): String {
+        return "articles/show"
     }
 }
