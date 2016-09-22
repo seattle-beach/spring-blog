6d9378f4ba344cf12807071fe3bf4cb91341fc2a
Andres Medina <amedina@pivotal.io>
2016-09-22 11:42:21 -0700

    Show articles
    Signed-off-by: Alpha Chen <achen@pivotal.io>

--- src/main/kotlin/com/arbitrarydefinitions/ArticleController.kt
+++ src/main/kotlin/com/arbitrarydefinitions/ArticleController.kt
@@ -3,6 +3,7 @@ package com.arbitrarydefinitions
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
+import org.springframework.web.bind.annotation.ModelAttribute
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestMethod
@@ -30,7 +31,9 @@ class ArticleController {
     }
 
     @RequestMapping("/{id}")
-    fun show(@PathVariable id: String): String {
+    fun show(@PathVariable id: Long, model: Model): String {
+        var article = repository.findOne(id)
+        model.addAttribute("article", article)
         return "articles/show"
     }
 }
